948f24a8758c4071186db1439873a183b23ade28
Dmytry <vuyko@ukr.net>
2020-11-12 16:21:11 +0200

    added endpoint for user snippets

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\Snippet\{SnippetController, StepController};
 use App\Http\Controllers\Auth\{SignInController, MeController, SignOutController};
+use App\Http\Controllers\Me\SnippetController as Me;
+use App\Http\Controllers\Snippet\{SnippetController, StepController};
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth'], function () {
@@ -11,12 +12,16 @@ Route::group(['prefix' => 'auth'], function () {
 });
 
 Route::group(['prefix' => 'snippets'], function () {
-    Route::get('/', [SnippetController::class, 'index']);
-    Route::post('/', [SnippetController::class, 'store']);
+    Route::get('', [SnippetController::class, 'index']);
+    Route::post('', [SnippetController::class, 'store']);
     Route::get('{snippet:uuid}', [SnippetController::class, 'show']);
     Route::patch('{snippet:uuid}', [SnippetController::class, 'update']);
 
     Route::patch('{snippet:uuid}/steps/{step:uuid}', [StepController::class, 'update']);
     Route::post('{snippet:uuid}/steps', [StepController::class, 'store']);
     Route::delete('{snippet:uuid}/steps/{step:uuid}', [StepController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'me'], function () {
+    Route::get('snippets', [Me::class, 'index']);
 });
